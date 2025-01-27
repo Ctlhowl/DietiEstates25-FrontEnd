ea67a76d9d981332765ea3b8ae48abbaed10ff0a
Fabrizio Ciotola <fabrizio.ciot@gmail.com>
2025-01-27 12:44:39 +0100

    aggiunta libreria icone

--- frontend/src/app/components/backoffice/sidebar/sidebar.component.ts
+++ frontend/src/app/components/backoffice/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AppComponent } from "../../../app.component";
 import { RouterOutlet } from '@angular/router';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [AppComponent, RouterOutlet],
+  imports: [AppComponent, RouterOutlet, NgIcon],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
